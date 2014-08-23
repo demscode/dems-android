af42fb46e21af3ddbd31467cd1de9707d2080531
Alex Armatys <aarmatys@outlook.com>
2014-08-23 14:51:50 +1000

    Configured tests and added example test (fails)

---
+++ dems/app/src/main/java/tests/ExampleTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+/**
+ * Created by Alex on 23/08/2014.
+ */
+public class ExampleTest {
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
+}
