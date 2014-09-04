01a23b97edfbffcab51e745adadfe6565414913c
Corey Bradford <Cbrad24@hotmail.com>
2014-09-04 14:07:40 +1000

    Fixes and content
    Added more content such as the ability to zoom in on location and update
    it. Also fixed a bug with location where it was using a past location.

--- app/src/main/java/com/example/cbrad24/mapexample2/MyActivity.java
+++ app/src/main/java/com/example/cbrad24/mapexample2/MyActivity.java
@@ -1,19 +1,26 @@
 package com.example.cbrad24.mapexample2;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MyActivity extends Activity {
     GPS gps;
     GoogleMap map;
+    TextView loctxt;
+    Marker patient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +29,28 @@ public class MyActivity extends Activity {
 
         gps = new GPS(MyActivity.this);
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        loctxt = (TextView) findViewById(R.id.header);
 
         if (gps.canGetLocation()) {
             double latitude = gps.getLatitude();
             double longitude = gps.getLongitude();
             LatLng position = new LatLng(latitude, longitude);
-            CameraUpdate update = CameraUpdateFactory.newLatLng( position );
+            patient = map.addMarker(new MarkerOptions().position(position).title("Patient"));
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(position, 16);
             map.animateCamera( update );
+            loctxt.setText( "Latitude:" +  latitude  + ", Longitude:"+ longitude );
         }
+    }
+
+    public void locate_onClick(View v) {
+        gps.getLocation();
+        double latitude = gps.getLatitude();
+        double longitude = gps.getLongitude();
+        LatLng position = new LatLng(latitude, longitude);
+        patient.setPosition(position);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(position, 16);
+        map.animateCamera( update );
+        loctxt.setText( "Latitude:" +  latitude  + ", Longitude:"+ longitude );
     }
 
     @Override
