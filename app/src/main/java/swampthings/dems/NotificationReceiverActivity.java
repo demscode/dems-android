78fa59d5beec19f956c3b97568c85d7c0e120bc9
Alex Armatys <aarmatys@outlook.com>
2014-09-16 09:45:16 +1000

    Basic notification implementation.

---
+++ app/src/main/java/swampthings/dems/NotificationReceiverActivity.java
@@ -0,0 +1,12 @@
+package swampthings.dems;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationReceiverActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
