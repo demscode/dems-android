639fd2ca7e495c3c3fa103d338659e05cabc6fdb
Alex Armatys <aarmatys@outlook.com>
2014-09-20 17:35:49 +1000

    Added alarm functionality to spring notification at appropriate time. Will need tweaking when RESTful functionaluity is added.

--- app/src/main/java/swampthings/dems/HomeActivity.java
+++ app/src/main/java/swampthings/dems/HomeActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.app.Notification;
+import android.app.AlarmManager;
+import android.app.Notification.*;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,20 +22,15 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.json.JSONObject;
 
-public class HomeActivity extends Activity {
-    /**  Tracking functionality "borrowed" from Stack Overflow, be sure to take what is needed but
-        change variable names etc.
-    */
-    /*
-    public class Tracking extends MapActivity implements LocationListener {
->>>>>>> Basic notification implementation.
+import org.json.JSONObject;
 
+import java.util.Calendar;
 
 public class HomeActivity extends Activity implements View.OnClickListener {
 
     protected String patientID;
     protected GPS gps;
-    protected String patientAPIURL = "http://demsweb.herokuapp.com/api/patient/";//"http://demsweb.herokuapp.com/api/patient/";
+    protected String patientAPIURL = "http://demsweb.herokuapp.com/api/patient/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +54,6 @@ public class HomeActivity extends Activity implements View.OnClickListener {
 
     }
 
-<<<<<<< HEAD
     @Override
     protected void onStart() {
         super.onStart();
@@ -65,27 +63,12 @@ public class HomeActivity extends Activity implements View.OnClickListener {
         if (gps.canGetLocation()) {
             gps.POSTLocation();
         }
-=======
         }
-       */
 
-    String id;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        // Grab the id parsed from LoginActivity
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            id = extras.getString("id");
-        }
-    }
-
-    public void notification() {
-        // Prepare intent which is triggered if the notification is selected
-        Intent intent = new Intent(this, NotificationReceiverActivity.class);
+    public void setNotification() {
+        // Intent to run notification
+        Intent intent = new Intent(this, Notification.class);
         PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
         // Build notification
@@ -105,7 +88,20 @@ public class HomeActivity extends Activity implements View.OnClickListener {
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, n);
->>>>>>> Basic notification implementation.
+        // Set alarm time here!
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+/*
+        calendar.set(Calendar.YEAR, );
+        calendar.set(Calendar.MONTH, );
+        calendar.set(Calendar.DAY_OF_MONTH, );
+        calendar.set(Calendar.HOUR_OF_DAY, );
+        calendar.set(Calendar.MINUTE, );
+        calendar.set(Calendar.SECOND, 0);
+ */
+        // AlarmManager will run the pending intent when date/time comes
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pIntent);
     }
 
 
@@ -198,4 +194,54 @@ public class HomeActivity extends Activity implements View.OnClickListener {
 
     }
 
+    protected class Notification extends Service {
+        @Override
+        public void onCreate() {
+        }
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+        }
+
+        // Not entirely sure this is correct - can't find api documentation
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            super.onStartCommand(intent, flags, startId);
+
+            // Prepare intent which is triggered if the notification is selected
+            intent = new Intent(this, NotificationReceiverActivity.class);
+            PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            // Build notification
+            // The addAction re-uses the same intent to keep the example short
+            android.app.Notification n  = new Builder(this)
+                    .setContentTitle("Reminder!")
+                    .setContentText("Subject")
+                    .setSmallIcon(R.drawable.reminder)
+                    .setContentIntent(pIntent)
+                    .setAutoCancel(true)
+                    .addAction(R.drawable.checkbox_colourful, "Ok", pIntent).build();
+            // Extra buttons if needed?
+            //        .addAction(R.drawable.alarm, "Sleep", pIntent)
+            //        .addAction(R.drawable.ic_launcher, "More", pIntent).build();
+
+            // Run notification
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(0, n);
+
+            return flags; // ??? - unsure what it wants me to return
+        }
+
+        @Override
+        public boolean onUnbind(Intent intent) {
+            return super.onUnbind(intent);
+        }
+    }
 }
